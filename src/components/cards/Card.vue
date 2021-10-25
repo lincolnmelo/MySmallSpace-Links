06d90b5b42cc90c5d42ef6420245b0f3bddd412f
Lincoln Melo <lincoln.melo@outlook.com.br>
2021-10-25 20:44:22 -0300

    Implementando retorno de items nos cards

--- src/components/cards/Card.vue
+++ src/components/cards/Card.vue
@@ -1,20 +1,64 @@
 <template>
-  <div class="CardComponent">
-    <p>
-      Teste Card
-    </p>
-  </div>
+    <div class="card pink">
+      <h2>{{title}}</h2>
+      <p>{{label}}</p>
+      <img
+        class="image"
+        v-bind:src="`${imgUrl}`"
+        alt=""
+      />
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'Card',
-}
+  name: "Card",
+  props: {
+    title: String,
+    label: String,
+    imgUrl: String
+  }
+};
 </script>
 
 <style scoped>
 .CardComponent {
- color: #ffc4ca;
- font-weight: bold;
+  color: #ffc4ca;
+  font-weight: bold;
+}
+
+body {
+  font-family: "Poppins", sans-serif;
+  background-color: #fafafa;
+}
+
+.card {
+  border-radius: 5px;
+  box-shadow: 7px 7px 13px 0px rgba(50, 50, 50, 0.22);
+  padding: 15px;
+  margin: 15px;
+  width: 400px;
+  min-height: 170px;
+  transition: all 0.3s ease-out;
+}
+
+.card:hover {
+  transform: translateY(-5px);
+  cursor: pointer;
+}
+
+.card p {
+  color: #a3a5ae;
+  font-size: 16px;
+}
+
+.image {
+  float: right;
+  max-width: 64px;
+  max-height: 64px;
+}
+
+.pink {
+  border-left: 3px solid #ffc4ca;
 }
 </style>
--- src/components/cards/index.vue
+++ src/components/cards/index.vue
@@ -1,17 +1,37 @@
 <template>
-  <Card/>
+  <div class="row">
+    <Card
+      :title="item.title"
+      :label="item.label"
+      :imgUrl="item.imgUrl"
+      v-for="item in indications"
+      :key="item.title"
+    />
+  </div>
 </template>
 
 <script>
-import Card from '../cards/Card.vue';
+import Card from "../cards/Card.vue";
+import { Indications } from "../../services/getIndications";
 
 export default {
-  name: 'Cards',
-  props: {
-    msg: String
-  },
+  name: "Cards",
   components: {
-    Card
-  }
-}
+    Card,
+  },
+  setup() {
+    const indications = Indications;
+    return { indications };
+  },
+};
 </script>
+
+<style scoped>
+.row {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  flex-basis: 100%;
+}
+</style>
